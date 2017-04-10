67b849056287023812d11004d82ff23caf3c3e5c
Kristi Bryden <kristibryden@gmail.com>
2017-04-10 17:16:24 -0600

    fixed some bugs
    added some designs, fixed buttons, changed images

--- DEV/script.js
+++ DEV/script.js
@@ -37,3 +37,36 @@ function showDivs(n) {
     }
     x[slideIndex-1].style.display = "block";
 }
+
+//http://thenewcode.com/777/Create-Fullscreen-HTML5-Page-Background-Video
+var closeButton = document.querySelector('.close-icon');
+closeButton.addEventListener('click', function() {
+    console.log('close it!');
+    document
+      .querySelector('body')
+      .classList
+      .toggle('nav-open');
+});
+
+var vid = document.getElementById("bgvid"),
+pauseButton = document.getElementById("vidpause");
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+vid.addEventListener("ended", function() {
+    // only functional if "loop" is removed
+     vid.pause();
+  // to capture IE10
+  vidFade();
+});
+
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+  if (vid.paused) {
+vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+  }
+})
