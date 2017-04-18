a7d42055af592a0065e10d0b70354e57239a22d5
Kristi Bryden <kristibryden@gmail.com>
2017-04-17 20:32:44 -0600

    some changes to sass
    correct overflow on body

--- DEV/script.js
+++ DEV/script.js
@@ -52,21 +52,4 @@ var vid = document.getElementById("bgvid"),
 pauseButton = document.getElementById("vidpause");
 function vidFade() {
     vid.classList.add("stopfade");
-}
-vid.addEventListener("ended", function() {
-    // only functional if "loop" is removed
-     vid.pause();
-  // to capture IE10
-  vidFade();
-});
-
-pauseButton.addEventListener("click", function() {
-    vid.classList.toggle("stopfade");
-  if (vid.paused) {
-vid.play();
-    pauseButton.innerHTML = "Pause";
-  } else {
-        vid.pause();
-        pauseButton.innerHTML = "Paused";
-  }
-})
+};
